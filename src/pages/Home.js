82716b54ff14890c6a0f4c77662251ca6351922c
aesandino <anabellasandino@gmail.com>
2023-10-20 11:26:10 -0700

    added styling and navlink

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,15 @@ import React from "react"
 
 const Home = () => {
     return(
-        <> <h1> May the odds be ever in your favor! </h1>
-        </>
+        <> 
+        <h1> May the odds be ever in your favor! </h1>
+     
+         <img className= "picture" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqPuuzDEDJ4PlTxXNYzMa6ynEE8_r5svvxWg&usqp=CAU">
+              
+         </img>
+          </>
+
+
     )
 
 }
